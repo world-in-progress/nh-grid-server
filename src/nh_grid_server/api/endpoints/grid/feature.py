30674c32e2bf3fae8c7eaf54c9798494bc6bbb84
Stevnda <241302111@njnu.edu.cn>
2025-06-03 22:33:17 +0800

    feat: enhance feature upload and save functionality with new methods and improved serialization
    - Updated upload_feature and save_uploaded_feature methods to include feature_type parameter.
    - Introduced new methods for saving features and converting shapefiles to GeoJSON.
    - Enhanced serialization in UploadInfo and UploadedFeatureSaveInfo to accommodate new parameters.
    - Added new schemas for handling uploaded feature data in the API.

--- src/nh_grid_server/api/endpoints/grid/feature.py
+++ src/nh_grid_server/api/endpoints/grid/feature.py
@@ -4,7 +4,7 @@ from icrms.ifeature import IFeature
 from ....core.config import settings
 from fastapi import APIRouter, Response, HTTPException, Body
 import json
-from ....schemas.feature import UploadBody
+from ....schemas.feature import UploadBody, FeatureSaveBody, UploadedFeatureSaveBody
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -16,9 +16,9 @@ router = APIRouter(prefix='/feature', tags=['grid / feature'])
 def upload_feature(body: UploadBody=Body(..., description='upload feature info')):
     with cc.compo.runtime.connect_crm(settings.FEATURE_TCP_ADDRESS, IFeature) as feature_interface:
 
-        logger.info(f'Uploading feature: {body.file_path} {body.file_type}')
+        logger.info(f'Uploading feature: {body.file_path} {body.file_type} {body.feature_type}')
 
-        upload_info = feature_interface.upload_feature(body.file_path, body.file_type)
+        upload_info = feature_interface.upload_feature(body.file_path, body.file_type, body.feature_type)
 
         logger.info(f'Uploading feature info: {upload_info}')
         
@@ -28,9 +28,18 @@ def upload_feature(body: UploadBody=Body(..., description='upload feature info')
         )
 
 @router.post('/save')
-def save_feature(feature_json: str, is_edited: bool):
+def save_feature(body: FeatureSaveBody=Body(..., description='save feature info')):
     with cc.compo.runtime.connect_crm(settings.FEATURE_TCP_ADDRESS, IFeature) as feature_interface:
-        save_info = feature_interface.save_vector_feature(feature_json, is_edited)
+        save_info = feature_interface.save_feature(body.feature_name, body.feature_type, body.feature_json)
+        return Response(
+            content=json.dumps(save_info),
+            media_type='application/json'
+        )
+    
+@router.post('/save_uploaded')
+def save_uploaded_feature(body: UploadedFeatureSaveBody=Body(..., description='save uploaded feature info')):
+    with cc.compo.runtime.connect_crm(settings.FEATURE_TCP_ADDRESS, IFeature) as feature_interface:
+        save_info = feature_interface.save_uploaded_feature(body.file_path, body.feature_type, body.feature_json, body.is_edited)
         return Response(
             content=json.dumps(save_info),
             media_type='application/json'
