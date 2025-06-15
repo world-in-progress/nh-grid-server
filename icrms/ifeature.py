38ffdd0f57286a6abeaf9085e352542f8d1fb87f
Stevnda <241302111@njnu.edu.cn>
2025-06-15 20:20:43 +0800

    feat: enhance feature management and API endpoints
    - Added support for feature metadata management by introducing a new `meta.json` file to store feature properties.
    - Updated methods in `feature.py` to streamline feature upload, save, and retrieval processes, including changes to method signatures for improved clarity.
    - Enhanced the `ifeature.py` interface to reflect the new feature management capabilities and updated serialization methods.
    - Introduced a new API endpoint to retrieve feature metadata and updated existing endpoints to align with the new method signatures.
    - Modified schemas in `feature.py` to include the new `FeatureProperty` model for better data structure representation.

--- icrms/ifeature.py
+++ icrms/ifeature.py
@@ -8,20 +8,19 @@ from src.nh_grid_server.schemas.feature import FeatureProperty, UpdateFeaturePro
 
 @cc.transferable
 class UploadInfo:
-    def serialize(file_path: str, file_type: str, feature_type: str) -> bytes:
+    def serialize(file_path: str, file_type: str) -> bytes:
         schema = pa.schema([
             pa.field('file_path', pa.string()),
             pa.field('file_type', pa.string()),
         ])
         data = {
             'file_path': file_path,
-            'file_type': file_type,
-            'feature_type': feature_type
+            'file_type': file_type
         }
         table = pa.Table.from_pylist([data], schema=schema)
         return cc.message.serialize_from_table(table)
 
-    def deserialize(arrow_bytes: bytes) -> tuple[str, str, str]:
+    def deserialize(arrow_bytes: bytes) -> tuple[str, str]:
         row = cc.message.deserialize_to_rows(arrow_bytes)[0]
         return (
             row['file_path'],
@@ -34,7 +33,6 @@ class UploadedFeatureSaveInfo:
                
         data = {
             'file_path': file_path,
-            'feature_type': feature_type,
             'feature_json': feature_json,
             'is_edited': is_edited
         }
@@ -93,10 +91,10 @@ class IFeature:
     =
     Interface of Core Resource Model (ICRM) specifies how to interact with CRM. 
     """
-    def upload_feature(self, file_path: str, file_type: str, feature_type: str) -> dict[str, bool | str]:
+    def upload_feature(self, file_path: str, file_type: str) -> dict[str, bool | str]:
         ...
 
-    def save_uploaded_feature(self, file_path: str, feature_type: str, feature_json: dict[str, Any], is_edited: bool) -> dict[str, bool | str]:
+    def save_uploaded_feature(self, file_path: str, feature_json: dict[str, Any], is_edited: bool) -> dict[str, bool | str]:
         ...
 
     def save_uploaded_feature(self, file_path: str, feature_json: dict[str, Any], is_edited: bool) -> dict[str, bool | str]:
