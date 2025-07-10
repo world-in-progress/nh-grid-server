7f1f2a15f24afd41151fb111e514c4e1fc4ae3c8
YcSoku <dsssyc02@gmail.com>
2025-07-10 20:40:05 +0800

    feat: update create_patch API to use schema_name instead of project_name and adjust patch directory structure

--- src/nh_grid_server/api/endpoints/patch.py
+++ src/nh_grid_server/api/endpoints/patch.py
@@ -13,71 +13,38 @@ from icrms.itreeger import ReuseAction
 
 router = APIRouter(prefix='/patch')
 
-@router.post('/{project_name}', response_model=BaseResponse)
-def create_patch(project_name: str, patch_data: PatchMeta):
+@router.post('/{schema_name}', response_model=BaseResponse)
+def create_patch(schema_name: str, patch_data: PatchMeta):
     """
     Description
     --
-    Create a patch belonging to a specified project.
+    Create a patch belonging to a specified schema.
     """
 
-    # Check if the project directory exists
-    project_dir = Path(settings.GRID_PROJECT_DIR, project_name)
-    if not project_dir.exists():
-        raise HTTPException(status_code=404, detail=f'Grid project ({project_name}) not found')
-
-    try:
-        project_meta_file = project_dir / settings.GRID_PROJECT_META_FILE_NAME
-        with open(project_meta_file, 'r') as f:
-            project_data = json.load(f)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f'Failed to read project meta file: {str(e)}')
+    # Check if the schema directory exists
+    grid_schema_path = Path(settings.GRID_SCHEMA_DIR, schema_name, 'schema.json')
+    if not grid_schema_path.exists():
+        raise HTTPException(status_code=404, detail=f'Grid schema ({schema_name}) not found')
     
-    project_meta = ProjectMeta(**project_data)
-    project_path = Path(settings.GRID_PROJECT_DIR, project_meta.name)
-
-    # Check if schema is valid
-    schema_file_path = Path(settings.GRID_SCHEMA_DIR) / f'{project_meta.schema_name}.json'
-    if not schema_file_path.exists():
-        raise FileNotFoundError(f'Schema file {schema_file_path} does not exist')
-
-    # Check if the patch directory already exists
-    patch_dir = project_dir / patch_data.name
-    if patch_dir.exists():
-        return BaseResponse(
-            success=False,
-            message='Grid patch already exists. Please use a different name.'
-        )
-
-    # Write the patch meta information to a file
-    patch_dir.mkdir(parents=True, exist_ok=True)
-    patch_meta_file = patch_dir / settings.GRID_PATCH_META_FILE_NAME
     try:
+        
+        grid_patch_path = Path(settings.GRID_SCHEMA_DIR, schema_name, 'patches', patch_data.name)
+        if grid_patch_path.exists():
+            return BaseResponse(
+                success=False,
+                message='Grid patch already exists. Please use a different name.'
+            )
+
+        # Write the patch meta information to a file
+        grid_patch_path.mkdir(parents=True, exist_ok=True)
+        patch_meta_file = grid_patch_path / settings.GRID_PATCH_META_FILE_NAME
+        
         with open(patch_meta_file, 'w') as f:
             f.write(patch_data.model_dump_json(indent=4))
-            node_key = f'root/projects/{project_name}/{patch_data.name}'
+            node_key = f'root.topo.schemas.{schema_name}.patches.{patch_data.name}'
 
             # Mount the patch node
             BT.instance.mount_node('patch', node_key)
-            
-            # Mount child nodes
-            # - topo
-            BT.instance.mount_node(
-                'topo', f'{node_key}/topo',
-                {
-                    'temp': settings.GRID_PATCH_TEMP,
-                    'schema_file_path': str(schema_file_path),
-                    'grid_project_path': str(project_path / patch_data.name),
-                    'meta_file_name': settings.GRID_PATCH_META_FILE_NAME,
-                }
-            )
-            # - feature
-            BT.instance.mount_node(
-                'feature', f'{node_key}/feature',
-                {
-                    'feature_path': str(project_path / patch_data.name / 'feature'),
-                }
-            )
             
     except Exception as e:
         raise HTTPException(status_code=500, detail=f'Failed to create grid patch: {str(e)}')
