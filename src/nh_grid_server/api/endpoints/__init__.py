c7febf20eddc83bb4ff1d330a2530226d7735ae9
StevenDa <149681622+Stevnda@users.noreply.github.com>
2025-07-17 14:42:22 +0800

    Dev_dmk (#11)
    * feat: implement Solution class and related API endpoints for solution management
    
    * feat: implement Solution class and related API endpoints for solution management
    
    * Merge branch 'dev_dmk' of https://github.com/world-in-progress/nh-grid-server into dev_dmk

--- src/nh_grid_server/api/endpoints/__init__.py
+++ src/nh_grid_server/api/endpoints/__init__.py
@@ -4,6 +4,8 @@ from . import feature
 from . import scene
 from . import patch
 from . import topo
+from . import proxy
+from . import solution
 
 router = APIRouter(prefix='/api', tags=['api'])
 
@@ -12,3 +14,5 @@ router.include_router(patch.router)
 router.include_router(topo.router)
 router.include_router(feature.router)
 router.include_router(scene.router)
+router.include_router(proxy.router)
+router.include_router(solution.router)
