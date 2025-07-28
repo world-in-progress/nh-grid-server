c960c2a3557f0365d25884094794d03bb6326b9c
Stevnda <1849698643@qq.com>
2025-07-28 20:21:44 +0800

    feat: implement human action management in Solution class with CRUD operations

--- icrms/isolution.py
+++ icrms/isolution.py
@@ -1,13 +1,9 @@
 import c_two as cc
-from pydantic import BaseModel
-
-class CreateSolutionBody(BaseModel):
-    name: str
-    env: dict
 
 @cc.icrm
 class ISolution:
 
+    # Model Server to Resource Server
     def clone_env(self) -> dict:
         """
         克隆环境变量
@@ -18,6 +14,45 @@ class ISolution:
     def get_env(self) -> dict:
         """
         获取环境变量字典
-        :return: 环境变量
+        :return: 环境变量字典
+        """
+        ...
+
+    def get_action_types(self) -> list[str]:
+        """
+        获取动作类型列表
+        :return: 动作类型列表
+        """
+        ...
+
+    def add_human_action(self, action_type: str, params: dict) -> str:
+        """
+        添加人工操作
+        :param action: 人工操作参数
+        :return: 添加结果
+        """
+        ...
+
+    def update_human_action(self, action_id: str, params: dict) -> None:
+        """
+        更新人工操作
+        :param action_id: 人工操作ID
+        :param params: 人工操作参数
+        :return: None
+        """
+        ...
+
+    def delete_human_action(self, action_id: str) -> None:
+        """
+        删除人工操作
+        :param action_id: 人工操作ID
+        :return: None
+        """
+        ...
+
+    def get_human_actions(self) -> list[dict]:
+        """
+        获取所有人工操作
+        :return: 人工操作列表
         """
         ...
